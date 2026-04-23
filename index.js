25fc9fcc3cc3f16efb440f9497221eefda819863
Weston Hopkins <whopkins@anamosa.k12.ia.us>
2026-04-23 18:07:56 +0000

    please no storm

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function guessNumber(guess) {
+    if (guess != 25) {
+        return "not it";
+    }
+}
 
+function strictGuessNumber(guess) {
+    if (guess !== 25) {
+        return "not it";
+    }
+}
